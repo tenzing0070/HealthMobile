d457e8e7b61813d9c02535477e481d60ca4d474d
tenzing0070 <tenzing0070@gmail.com>
2020-05-16 19:59:35 +0545

    profile fragment design updates

--- app/src/main/java/com/dawa/fragment/ProfileFragment.java
+++ app/src/main/java/com/dawa/fragment/ProfileFragment.java
@@ -10,17 +10,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import com.dawa.api.health_api;
 import com.dawa.mobilehealth.R;
 import com.dawa.mobilehealth.login.LoginActivity;
 import com.dawa.mobilehealth.login.UpdateProfileActivity;
+import com.dawa.model.users;
+import com.dawa.url.url;
+import com.squareup.picasso.Picasso;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class ProfileFragment extends Fragment {
+
+    private EditText firstname, lastname;
+    ImageView imgProfile;
 
     public ProfileFragment() {
 
@@ -33,8 +47,14 @@ public class ProfileFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        firstname = v.findViewById(R.id.txtfirstname);
+        lastname = v.findViewById(R.id.txtlastname);
+        imgProfile = v.findViewById(R.id.imgProfilee);
+
+        openuserinfo();
 
         Button btnMyProfile = v.findViewById(R.id.btnMyProfile);
+
         btnMyProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,8 +72,8 @@ public class ProfileFragment extends Fragment {
 //                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MapsActivity()).commit();
 //            }
 //        });
-
-
+//
+//
 //        Button btnFeedback = v.findViewById(R.id.btnFeedback);
 //        btnFeedback.setOnClickListener(new View.OnClickListener() {
 //
@@ -98,10 +118,55 @@ public class ProfileFragment extends Fragment {
 
         });
 
-
         return v;
+
+
     }
-}
+        private void openuserinfo() {
+
+            health_api hrsApi = url.getInstance().create(health_api.class);
+            Call<users> usersCall = hrsApi.getUserDetails(url.token);
+            System.out.println("token is:"+url.token);
+
+            usersCall.enqueue(new Callback<users>() {
+                @Override
+                public void onResponse(Call<users> call, Response<users> response) {
+                    if (!response.isSuccessful()) {
+                        Toast.makeText(getActivity(), "code" + response.code(), Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    if(response.body().getImage()!=null) {
+                        String imgpath = url.BASE_URL + response.body().getImage();
+                        System.out.println("image response is :"+imgpath);
+
+                        Picasso.get().load(imgpath).into(imgProfile);
+
+                    }
+                    else
+                    {
+                        Picasso.get().load(R.drawable.image1).into(imgProfile);
+                    }
+
+                    firstname.setText(response.body().getFirstname());
+                    lastname.setText(response.body().getLastname());
+
+                }
+
+                @Override
+                public void onFailure(Call<users> call, Throwable t) {
+                    Toast.makeText(getActivity(), "Error" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+
+
+
+
+        //return v;
+    }
+
+
 
 
 
