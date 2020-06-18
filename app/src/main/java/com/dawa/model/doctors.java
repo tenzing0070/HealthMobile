35be8bb3dc7ac8f22a73dd8e60d74f54797e8b23
tenzing0070 <tenzing0070@gmail.com>
2020-06-18 08:20:16 +0545

    doc model created

---
+++ app/src/main/java/com/dawa/model/doctors.java
@@ -0,0 +1,77 @@
+package com.dawa.model;
+
+public class doctors {
+    private String _id;
+    
+    private String firstname;
+    private String lastname;
+    private String specialist;
+    private String image;
+    private String gender;
+    private String price;
+
+    public doctors(String firstname, String lastname, String specialist, String image, String gender, String price ) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.specialist = specialist;
+        this.image = image;
+        this.gender = gender;
+        this.price = price;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSpecialist() {
+        return specialist;
+    }
+
+    public void setSpecialist(String specialist) {
+        this.specialist = specialist;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+}
